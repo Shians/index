88c2919db4bc665f08e65cb82fdfe2895c40b729
Shians <registertonysu@gmail.com>
2019-05-01 14:10:21 +1000

    Updated example to use system.file

--- inst/extdata/example.R
+++ inst/extdata/example.R
@@ -2,9 +2,9 @@ library(limma)
 library(edgeR)
 library(Homo.sapiens)
 
-exon <- readRDS("inst/extdata/exon_dge.Rds")
-intron <- readRDS("inst/extdata/intron_dge.Rds")
-group <- readRDS("inst/extdata/group.Rds")
+exon <- readRDS(system.file("extdata/exon_dge.Rds", package = "INdEX"))
+intron <- readRDS(system.file("extdata/intron_dge.Rds", package = "INdEX"))
+group <- readRDS(system.file("extdata/group.Rds", package = "INdEX"))
 
 x <- index_analysis(exon, intron, group)
 plot_voom(x)
