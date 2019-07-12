7eb5180ebd1fc2c745c5820df24fb6131d43c9ba
Shians <registertonysu@gmail.com>
2019-07-12 13:39:36 +1000

    Added option for t-statistics or logFC plot, default changed to t-statistic

--- R/plot_index.R
+++ R/plot_index.R
@@ -1,13 +1,15 @@
 #' Create main INdEX analysis plots
 #'
 #' @param index_output the output from running index_analysis
+#' @param type the type of value to plot between exon and introns (default: "tstat")
 #'
 #' @return None
 #' @export
 #'
 #' @examples
-plot_index <-
-function(index_output) {
+plot_index <- function(index_output, type = c("tstat", "logfc")) {
+    type <- match.arg(type)
+
     par(mfrow = c(1, 2))
     on.exit(par(mfrow = c(1, 1)))
 
@@ -36,15 +38,27 @@ function(index_output) {
         main = "INdEX Categories"
     )
 
-    plot(
-        index_output$tops$intron$logFC,
-        index_output$tops$exon$logFC,
-        pch = 20,
-        col = point_col,
-        main = "logFC",
-        xlab = "Intron logFC",
-        ylab = "Exon logFC"
-    )
+    if (type == "tstat") {
+        plot(
+            index_output$tops$intron$t,
+            index_output$tops$exon$t,
+            pch = 20,
+            col = point_col,
+            main = "t-statistic",
+            xlab = "Intron t-statistic",
+            ylab = "Exon t-statistic"
+        )
+    } else if (type == "logfc") {
+        plot(
+            index_output$tops$intron$logFC,
+            index_output$tops$exon$logFC,
+            pch = 20,
+            col = point_col,
+            main = "logFC",
+            xlab = "Intron logFC",
+            ylab = "Exon logFC"
+        )
+    }
     abline(a = 0, b = 1, col = "#BBBBBB", lwd = 2)
     par(mfrow = c(1, 1))
 }
